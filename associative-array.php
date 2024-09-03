86de31a21a145462d510eb97103cad1efba87279
miyasei36 <say.yah2011@gmail.com>
2024-09-03 22:01:49 +0900

    php課題提出

---
+++ associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP課題編</title>
+ </head>
+ 
+ <body>
+  <p>
+    <?php
+
+      $kadai_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+      print_r($kadai_data);
+    ?>
+  </p>
+</body>
+
+</html>
